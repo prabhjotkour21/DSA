a66ec24cb0c69af38c58010114d02f4d7ff9cb1c
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-08-01 17:07:22 +0530

    feat: add Valid Palindrome solution

---
+++ ValidPalindrome.js
@@ -0,0 +1,11 @@
+let s = "A man, a plan, a canal: Panama"
+
+function isPalindrome(s) {
+    let str = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+    console.log(str);
+    for (let i in str) {
+        if()
+    }
+    
+}
+isPalindrome(s)
