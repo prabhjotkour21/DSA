c918e5daeff18c0d0f0796b27608b044cfeb2e15
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-08-05 15:39:34 +0530

    chore: add object practice examples

---
+++ obj.js
@@ -0,0 +1,72 @@
+// let student = {}
+
+// student.name = "jot"
+// student["age"] = 25
+// student.city = "Delhi"
+// student.age=26
+
+// console.log(student);
+
+
+
+// let student = {
+//     name: "jot",
+//     age: 25,
+//     city: "Delhi"
+// }
+
+// console.log(student.age);
+
+// console.log(student["city"]);
+
+
+// let user = {
+//     username: "jot",
+//     score: 100,
+//     course: "DSA"
+// }
+
+// console.log(user.username)
+// console.log(user["score"])
+
+
+// let map = {
+//     apple: 2,
+//     banana: 1
+// }
+
+// if (map["mango"] !== undefined) {
+//     console.log("mil gya");
+    
+// } else {
+//     console.log("nhi mila")
+// }
+// console.log(map["date"]);
+
+
+let product = {
+    price: 500,
+    quantity: 2
+}
+
+if (product.price !== undefined) {
+    console.log("present",product.price);
+}
+if (product.name !== undefined) {
+    console.log("present");
+    
+    
+} else {
+ console.log("name not found");   
+}
+
+let number = 10
+
+let obj = {}
+
+obj[number] = "hello"
+
+console.log(obj)
+// hello
+
+console.log(obj[10])= hello
