68f9adb2090213a113b4aafe9f22639177a788ed
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-08-07 13:50:40 +0530

    refactor: optimize isomorphic strings solution

--- IsomorphicStrings.js
+++ IsomorphicStrings.js
@@ -1,10 +1,46 @@
-let s = "egg"
-let t = "add"
-let  map={}
-for (let i = 0; i < s.length; i++){
-    if (!map[s[i]]) {
-        map[s[i]]=1
+let s = "paper"
+let t = "title"
+
+function Check(s, t) {
+    
+
+    let mapst = {}
+    let mapts = {}
+    for (let i = 0; i < s.length; i++) {
+        if (!mapst[s[i]]) {
+        
+            mapst[s[i]] = t[i]
+           
+        }
     }
+    for (let i = 0; i < t.length; i++) {
+        if (!mapts[t[i]]) {
+            
+        
+            mapts[t[i]] = s[i]
+           
+        }
+    }
+    for (let i = 0; i < s.length; i++) {
+        if (mapst[s[i]]) {
+            if (mapst[s[i]] !== t[i]) {
+                return false
+            
+            } else if (mapts[t[i]] !== s[i]) {
+                return false
+            }
+        } else {
+            mapst[s[i]] = t[i]
+        }
+    }return true
 }
 
-console.log(map);
+console.log(Check(s,t));
+
+
+
+// {
+//     e: a,
+//     g: d,
+//     g: d,    
+// }
