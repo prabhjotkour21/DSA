9b52854ca2987e0d4595afa8bb066d1d7fce374f
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-08-08 14:40:11 +0530

    feat: add first unique character solution

---
+++ FirstUniqueCharacter.js
@@ -0,0 +1,19 @@
+let s = "lleedcode"
+function isUniqueChar(s) {
+    let map = {}
+    for (let i = 0; i < s.length; i++){
+        if (map[s[i]]) {
+            map[s[i]]++
+        } else {
+            map[s[i]]=1
+        }
+    }
+    for (let i = 0; i < s.length; i++){
+        if (map[s[i]] == 1) {
+            return i
+        }
+    }
+    
+}
+
+console.log(isUniqueChar(s))
