f8bbdf931a4b6e8191556b30fd1e546f76e90ee6
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-08-12 16:17:26 +0530

    feat: add group anagrams solution

---
+++ GroupAnagrams.js
@@ -0,0 +1,17 @@
+let str = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+function groupAnagrams(str) {
+    let map = {}
+    for (let i = 0; i < str.length; i++){
+        let s = str[i].split("").sort().join('')
+        if (map[s]) {
+            map[s].push(str[i])
+        } else {
+            map[s]=[str[i]]
+        }
+    }
+    return Object.entries(map)
+}
+
+
+console.log(groupAnagrams(str));
